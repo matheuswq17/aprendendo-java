1d934c00fee6039bf45f97a436555b5ed1be7515
Matheus <toledomatheus071@gmail.com>
2024-03-11 18:13:19 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Tipos primitivos
+        //byte, short, int , long (todas pra numeros inteiros, a diferenca entre cada uma e o tamanho do numero que consegue armazenar)
+        int idade;
+        idade = 18;
+        //float, double (tambem numericos, mas conseguem armazenar o numero depois da virgula, double consegue armazenar um numero maior que o , sendo o float mais inpreciso)
+        float altura=1.80f;
+        //char (armazena um unico caracter alfanumerico(A-Z, a-z) ou um número (0-9))
+        char genero = 'f';
+        //boolean (variavel que armazena verdadeiro ou falso)
+        boolean especial = false;
+
+        //Tipos de referencia
+        //String (texto ou paragrafo)
+        String nome = "Matheus Xavier";
+
+    }
+}
